72e70cc45c5ad02835dbe2df92fbc1cbd8ca3b3c
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-09-15 04:18:11 +0900

    ignore error

--- main.rs
+++ main.rs
@@ -7,9 +7,13 @@
 
 use arboard::Clipboard;
 use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+	process::exit(0);
+    }
     let text: &String = &args[1]; // ここ 0-indexed じゃないの罠すぎるだろ
     let mut clipboard = Clipboard::new().unwrap();
     clipboard.set_text(text).unwrap();
