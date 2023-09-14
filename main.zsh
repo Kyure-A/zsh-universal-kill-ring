3335ba71f2e433dbcc51fb725db4da15c4791bf2
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-09-15 04:35:17 +0900

    fix bug (path)

--- main.zsh
+++ main.zsh
@@ -3,7 +3,9 @@
 kill-line-arboard ()
 {
     zle kill-line
-    rust-script ./main.rs $CUTBUFFER
+    path="$(dirname "$0")"
+    exec="$path/main.rs"
+    rust-script $exec $CUTBUFFER
 }
 
 zle -N kill-line-arboard
